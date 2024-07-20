d969d0f8b7947588ad6c58023370a48575ad76f7
Kaustubh Bhalerao <bhalerao@soildiagnostics.com>
2024-07-20 14:33:19 -0500

    qrurl complete

--- src/index.js
+++ src/index.js
@@ -7,9 +7,48 @@
  *
  * Learn more at https://developers.cloudflare.com/workers/
  */
+var QRCode = require('qrcode')
+
+const htmlgen = (text, qrsvg, err="") => {
+	return `<!DOCTYPE html>
+			<body>
+			${text}
+			<div style="width: 2in">
+			${qrsvg}
+			</div>
+			<div>${err}</div>
+			</body>`;
+}
 
 export default {
-	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
+	async fetch(request) {
+		const url = new URL(request.url)
+		const prefix = "?url="
+		const req = url.search.replace(prefix, "")
+		let result; 
+		try {
+			result = await QRCode.toString(req)
+		} catch (err) {
+			const example = 'https://example.com'; 
+			result = await QRCode.toString(example)
+			const html = htmlgen(`Try these examples: 
+			<a href='${url.origin}/?url=${example}'>html</a>
+			<strong>or</strong> 
+			<a href='${url.origin}/api/?url=${example}'>api</a>`, result, err);
+			return new Response(html, {
+				headers: { "content-type": "text/html;charset=UTF-8" }
+			});
+		}
+
+		if (url.pathname === '/api/') {
+			return new Response(JSON.stringify({url:req, qr: result}), {
+				headers: { "content-type": "application/json" }
+			})
+		} else {
+			const html = htmlgen(req, result);
+			return new Response(html, {
+				headers: { "content-type": "text/html;charset=UTF-8" }
+			});
+		}
 	},
 };
